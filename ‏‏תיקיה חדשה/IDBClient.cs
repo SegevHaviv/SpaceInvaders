847b802db385de177547b1ca085fba3523a6dfbc
Segev Haviv <segevhaviv@gmail.com>
2018-04-25 12:49:51 +0300

    Add files via upload

---
+++ ‏‏תיקיה חדשה/IDBClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CynetWebApplication.Models
+{
+    interface IDBClient
+    {
+        void SaveWebsiteInfo(WebsiteInfo websiteInfo);
+        long GetDocumnetsCount();
+    }
+}
---
+++ ‏‏תיקיה חדשה/WebsiteAnalyzeRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CynetWebApplication.Models
+{
+    public class WebsiteAnalyzeRequest
+    {
+        public string Url { get; set; }
+    }
+}
