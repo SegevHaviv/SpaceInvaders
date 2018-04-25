b798bc5aab12cc5e1a8e7f9b3f4598bfd3152561
Segev Haviv <segevhaviv@gmail.com>
2018-04-25 12:01:19 +0300

    Add files via upload

---
+++ MongoDBClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace CynetWebApplication.Models
+{
+
+    // Wrapper for the MongoClient class.
+    public class MongoDBClient
+    {
+        private readonly MongoUrl mongoUrl = new MongoUrl("mongodb://localhost:27017/cynet");
+
+        // Saves the given WebsiteInfo to the DB
+        public void SaveWebsiteInfo(WebsiteInfo websiteInfo)
+        {
+            var client = new MongoClient(mongoUrl);
+            var db = client.GetDatabase(mongoUrl.DatabaseName);
+
+            var coll = db.GetCollection<WebsiteInfo>("websites_data");
+            coll.InsertOne(websiteInfo);
+        }
+
+
+        // Retrieves the amount of documents inserted to the DB.
+        public long GetDocumnetsCount()
+        {
+            var client = new MongoClient(mongoUrl);
+            var db = client.GetDatabase(mongoUrl.DatabaseName);
+ 
+            var coll = db.GetCollection<WebsiteInfo>("websites_data");
+            return coll.Count(Builders<WebsiteInfo>.Filter.Empty);
+        }
+
+    }
+}
